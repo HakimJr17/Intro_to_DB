5880bde1f24b46289ad69627695903d8dbec4229
Brian Miano <brianmiano1@gmail.com>
2025-06-29 06:00:16 +0100

    added Order Details table

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -25,4 +25,13 @@ CREATE TABLE Orders (
     customer_id INT NOT NULL,
     order_date DATE,
     FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
-    )
+    );
+    
+CREATE TABLE Order_Details (
+orderdetailid INT PRIMARY KEY,
+quantity DOUBLE, 
+order_id INT NOT NULL,
+book_id INT NOT NULL,
+FOREIGN KEY (order_id) REFERENCES Orders(order_id), 
+FOREIGN KEY (book_id) REFERENCES Books(book_id)
+)
