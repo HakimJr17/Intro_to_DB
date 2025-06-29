88a644e19b1e89595f6a8a41970837cc824ec130
Brian Miano <brianmiano1@gmail.com>
2025-06-29 05:36:34 +0100

    added the books table

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -4,3 +4,11 @@ CREATE TABLE Authors (
     author_id INT PRIMARY KEY,
     author_name VARCHAR(255)
     );
+    
+CREATE TABLE Books (
+book_id INT PRIMARY KEY,
+title VARCHAR(130) NOT NULL,
+author_id INT NOT NULL,
+price DOUBLE,
+publication_date DATE
+);
