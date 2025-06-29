a5b07bbfde07db76274e0c3b20fa85cfbcd72ffb
Brian Miano <brianmiano1@gmail.com>
2025-06-29 07:58:46 +0100

    added data to customers

---
+++ task_5.sql
@@ -0,0 +1,5 @@
+sql = "INSERT INTO Customers(customer_id, customer_name, email, address) VALUES (%s, %s, %s, %s)"
+val = (1, "Cole Baidoo", "cbaidoo@sandtech.com", "123 Happiness Ave.")
+
+mycursor.execute(sql,val)
+mydb.commit()
